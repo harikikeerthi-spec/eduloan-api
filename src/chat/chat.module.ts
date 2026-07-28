7375beb66e428aeb3795a1bb8b53d4559432fd9e
User <user@example.com>
2026-07-28 11:23:19 +0530

    Import forwardRef in chat.module.ts

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { TwilioService } from './twilio.service';
 import { ChatGateway } from './chat.gateway';
